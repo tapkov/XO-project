57663e3547ee3cc0f8898ef11764fef76df41f78
tapkov <wlad.pavliuk@gmail.com>
2015-11-11 14:53:44 +0100

    Field - create

---
+++ src/com/XOProject/controller/Field.java
@@ -0,0 +1,20 @@
+package com.XOProject.controller;
+
+import java.awt.*;
+
+/**
+ * Created by vlad on 11.11.15.
+ */
+public class Field {
+
+    final int sizeFigure;
+
+    public Field(int size) {
+        this.sizeFigure = size;
+    }
+
+    public void setFigure(Point point, Figure figure){}
+
+    public Figure getFigure(Point point){}
+
+}
