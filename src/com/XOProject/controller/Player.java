fc43acd794167e9f5300653993c0589ff13d94c3
tapkov <wlad.pavliuk@gmail.com>
2015-11-11 14:53:25 +0100

    Player - create

---
+++ src/com/XOProject/controller/Player.java
@@ -0,0 +1,25 @@
+package com.XOProject.controller;
+
+/**
+ * Created by vlad on 11.11.15.
+ */
+public class Player {
+
+    public final String namePlayer  ;
+
+    public final Figure playersFigure ;
+
+    public Player(String namePlayer, Figure playersFigure) {
+        this.namePlayer = namePlayer;
+
+        this.playersFigure = playersFigure;
+    }
+
+    public String getName(){
+        return namePlayer;
+}
+
+    public Figure getFigure() {
+        return playersFigure;
+    }
+}
