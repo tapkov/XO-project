20a99ba3ba5ef1e2d7973bc917723111a0fa832a
tapkov <wlad.pavliuk@gmail.com>
2015-11-11 17:22:16 +0100

    Test for class Player - create

---
+++ src/com/XOProject/controller/Game.java
@@ -0,0 +1,18 @@
+package com.XOProject.controller;
+
+/**
+ * Created by vlad on 11.11.15.
+ */
+public class Game {
+
+    private final static String NAME_THE_GAME = "TicTakToe";
+
+    Player getPlayer(){
+        return;
+    }
+
+    String getNameTheGame(){
+        return NAME_THE_GAME;
+    }
+
+}
