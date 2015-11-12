1cffbd7cd44cbf304e5a89383c8e1ab820d5c5c6
tapkov <wlad.pavliuk@gmail.com>
2015-11-12 12:15:23 +0100

    Field with check points

--- src/com/XOProject/controller/Field.java
+++ src/com/XOProject/controller/Field.java
@@ -8,6 +8,8 @@ import java.awt.*;
 public class Field {
 
     public static final int SIZE_FIELD = 3;
+    public static final int MAX_COORDINATE = SIZE_FIELD;
+    public static final int MIN_COORDINATE = 0;
 
     Figure[][] field = new Figure[SIZE_FIELD][SIZE_FIELD];
 
@@ -18,5 +20,14 @@ public class Field {
     public Figure getFigure(Point point){
         return field[point.x][point.y];
     }
+
+    private boolean checkPoint (final Point point){
+        return checkCoordinate(point.x) && checkCoordinate(point.y);
+    }
+
+    private boolean checkCoordinate (final int coordinate){
+        return coordinate < MAX_COORDINATE && coordinate > MIN_COORDINATE;
+    }
+
 
 }
