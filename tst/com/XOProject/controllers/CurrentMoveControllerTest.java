76b909419fc992ffe1bfaecb35fa952c302b8aff
tapkov <wlad.pavliuk@gmail.com>
2015-11-22 12:14:16 +0100

    CurrentMoveController with test when field is full, and not winner

--- tst/com/XOProject/controllers/CurrentMoveControllerTest.java
+++ tst/com/XOProject/controllers/CurrentMoveControllerTest.java
@@ -41,4 +41,24 @@ public class CurrentMoveControllerTest {
         assertEquals(expectedValue, currentMoveController.currentMove(field));
     }
 
+    @Test
+    public void testCurrentMoveWhenFieldIsFull() throws Exception {
+        final Field field = new Field(3);
+        final CurrentMoveController currentMoveController = new CurrentMoveController();
+
+        field.setFigure(new Point(0,0), Figure.O);
+        field.setFigure(new Point(1,0), Figure.O);
+        field.setFigure(new Point(2,0), Figure.X);
+        field.setFigure(new Point(0,1), Figure.X);
+        field.setFigure(new Point(1,1), Figure.X);
+        field.setFigure(new Point(2,1), Figure.O);
+        field.setFigure(new Point(0,2), Figure.X);
+        field.setFigure(new Point(1,2), Figure.X);
+        field.setFigure(new Point(2,2), Figure.X);
+
+        assertNull(currentMoveController.currentMove(field));
+    }
+
+
+
 }
